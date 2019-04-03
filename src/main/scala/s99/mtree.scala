ad3a779a063d198a7643d3567da1d55bc6e92b4b
wqkant <wqkant@gmail.com>
2019-04-04 00:08:25 +0800

    add mtree

--- src/main/scala/s99/mtree.scala
+++ src/main/scala/s99/mtree.scala
@@ -7,7 +7,18 @@ case class MTree[+T](value: T, children: List[MTree[T]]) {
 
   override def toString = value.toString + children.map(_.toString + "^").mkString("")
 
-  def nodeCount(): Int = children.foldLeft(1)(_ + _.nodeCount())
+  def nodeCount: Int = children.foldLeft(1)(_ + _.nodeCount)
+
+  def internalPathLength: Int = children.foldLeft(0)((r, c) => c.nodeCount + c.internalPathLength + r)
+
+  def postorder: List[T] = children match {
+    case Nil => List(value)
+    case (h :: s) => h.postorder ::: s.flatMap(_.postorder) ::: List(value)
+  }
+
+  def postorder1: List[T] = children.foldRight(List(value))((r, c) => r.postorder1 ::: c)
+
+  def postorder2: List[T] = children.flatMap(_.postorder2) ::: List(value)
 }
 
 object MTree {
@@ -15,12 +26,14 @@ object MTree {
     def nextStrBound(pos: Int, nesting: Int): Int =
       if (nesting == 0) pos
       else nextStrBound(pos + 1, if (s(pos) == '^') nesting - 1 else nesting + 1)
+
     def splitChildStrings(pos: Int): List[String] =
       if (pos >= s.length) Nil
       else {
         val end = nextStrBound(pos + 1, 1)
         s.substring(pos, end - 1) :: splitChildStrings(end)
       }
+
     MTree(s(0), splitChildStrings(1).map(string2MTree(_)))
   }
 
@@ -29,8 +42,11 @@ object MTree {
   def apply[T](value: T, children: List[MTree[T]]) = new MTree(value, children)
 
   def main(args: Array[String]): Unit = {
-    println("afg^^c^bd^e^^".nodeCount())
-    println(MTree('a', List(MTree('f', List(MTree('g'))), MTree('c'), MTree('b', List(MTree('d'), MTree('e'))))).toString)
+    println("afg^^c^bd^e^^".postorder2)
+    println("afg^^c^bd^e^^".postorder1)
+    println("afg^^c^bd^e^^".postorder)
+    println("afg^^c^bd^e^^".internalPathLength)
+    // println(MTree('a', List(MTree('f', List(MTree('g'))), MTree('c'), MTree('b', List(MTree('d'), MTree('e'))))).toString)
     //    println(MTree('a', List(MTree('f'))).nodeCount)
     //    println(MTree('a', List(MTree('f', List(MTree('g'))), MTree('c'), MTree('b', List(MTree('d'), MTree('e'))))).toString)
   }
