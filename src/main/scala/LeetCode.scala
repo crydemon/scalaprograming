867a4196f9a1f5b6de68c4bc4a0bfd6d834309fe
wqkant <wqkant@gmail.com>
2019-04-24 23:43:59 +0800

    add 2

--- src/main/scala/LeetCode.scala
+++ src/main/scala/LeetCode.scala
@@ -17,7 +17,47 @@ object LeetCode {
     go(0, nums.length - 1, one)
   }
 
+
+  class ListNode(var _x: Int = 0) {
+    var next: ListNode = null
+    var x: Int = _x
+
+    override def toString: String = {
+      x + (if (next == null) "" else ", " + next.toString)
+    }
+  }
+
+  def addTwoNumbers(l1: ListNode, l2: ListNode): ListNode = {
+    def go(l1: ListNode, l2: ListNode, carry: Int, result: ListNode): ListNode = {
+      if (l1 == null && l2 == null && carry == 0) result
+      else {
+        val sum = ((if (l1 == null) 0 else l1.x) + (if (l2 == null) 0 else l2.x) + carry)
+        result.x = sum % 10
+        val carryNext: Int = sum / 10
+        val (l1Next , l2Next) = (if (l1 == null) null else l1.next, if (l2 == null) null else l2.next)
+        if (l1Next == null && l2Next == null && carryNext == 0) result
+        else {
+          result.next = new ListNode()
+          go(l1Next, l2Next, carryNext, result.next)
+        }
+      }
+    }
+
+    val result = new ListNode()
+    go(l1, l2, 0, result)
+    result
+  }
+
   def main(args: Array[String]): Unit = {
-    println(twoSum(Array(2, 5, 5, 11), 10).toList)
+    val l1 = new ListNode(2)
+    l1.next = new ListNode(4)
+    l1.next.next = new ListNode(3)
+    val l2 = new ListNode(5)
+    l2.next = new ListNode(6)
+    l2.next.next = new ListNode(4)
+    println(l1.toString)
+    println(l2.toString)
+    println(addTwoNumbers(l1, l2))
+    //println(twoSum(Array(2, 5, 5, 11), 10).toList)
   }
 }
